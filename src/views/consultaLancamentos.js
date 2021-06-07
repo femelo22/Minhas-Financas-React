93218cd6cb0aab5627da7adbdc47e69aea008b2b
luiz fernando <luiz123jfmg@gmail.com>
2021-06-07 17:58:37 -0300

    Implementando Datatable de lançamentos

--- src/views/consultaLancamentos.js
+++ src/views/consultaLancamentos.js
@@ -3,31 +3,37 @@ import { withRouter } from 'react-router-dom';
 import Card from '../components/card';
 import FormGroup from '../components/form-group';
 import SelectMenu from '../components/selectMenu';
+import LancamentoTable from '../views/lancamentosTable';
 
 class ConsultaLancamentos extends React.Component {
 
-    render(){
+    render() {
 
         const meses = [
-            { label: 'Selecione...', value: ''},
-            { label: 'Janeiro', value: 1},
-            { label: 'Fevereiro', value: 2},
-            { label: 'Março', value: 3},
-            { label: 'Abril', value: 4},
-            { label: 'Maio', value: 5},
-            { label: 'Junho', value: 6},
-            { label: 'Julho', value: 7},
-            { label: 'Agosto', value: 8},
-            { label: 'Setembro', value: 9},
-            { label: 'Outubro', value: 10},
-            { label: 'Novembro', value: 11},
-            { label: 'Dezembro', value: 12},
+            { label: 'Selecione...', value: '' },
+            { label: 'Janeiro', value: 1 },
+            { label: 'Fevereiro', value: 2 },
+            { label: 'Março', value: 3 },
+            { label: 'Abril', value: 4 },
+            { label: 'Maio', value: 5 },
+            { label: 'Junho', value: 6 },
+            { label: 'Julho', value: 7 },
+            { label: 'Agosto', value: 8 },
+            { label: 'Setembro', value: 9 },
+            { label: 'Outubro', value: 10 },
+            { label: 'Novembro', value: 11 },
+            { label: 'Dezembro', value: 12 },
         ]
 
         const tipos = [
-            { label: 'Selecione...', value: ''},
-            { label: 'Despesa', value: 'DESPESA'},
-            { label: 'Receita', value: 'RECEITA'},
+            { label: 'Selecione...', value: '' },
+            { label: 'Despesa', value: 'DESPESA' },
+            { label: 'Receita', value: 'RECEITA' },
+        ]
+
+        const lancamentos = [
+            { descricao: 'Salario', valor: 5000, mes: 1, tipo: 'Receita', status: 'Efetivado' }
+           
         ]
 
 
@@ -38,21 +44,32 @@ class ConsultaLancamentos extends React.Component {
                         <div className="bs-component">
 
                             <FormGroup htmlFor="inputAno" label="Ano: *">
-                                <input type="text" 
-                                    className="form-control" 
-                                    id="inputAno" 
-                                    aria-describedby="emailHelp" 
+                                <input type="text"
+                                    className="form-control"
+                                    id="inputAno"
+                                    aria-describedby="emailHelp"
                                     placeholder="Digite o Ano" />
-                            </FormGroup>< br/>
-                            
+                            </FormGroup>< br />
+
                             <FormGroup htmlFor="inputMes" label="Mês: ">
                                 <SelectMenu id="inputMes" className="form-control" lista={meses} />
-                            </FormGroup>< br/>
+                            </FormGroup>< br />
 
                             <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: ">
                                 <SelectMenu id="inputTipo" className="form-control" lista={tipos} />
                             </FormGroup>
-                        </div>  
+                            < br/>
+                            <button type="button" className="btn btn-success">Buscar</button>
+                            <button type="button" className="btn btn-danger">Cadastrar</button>
+
+                        </div>
+                    </div>
+                </div><br/>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="bs-component">
+                            <LancamentoTable lancamentos={lancamentos}/>
+                        </div>
                     </div>
                 </div>
             </Card>
